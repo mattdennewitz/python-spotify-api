f4e0558e9cd6b3e1d4fc2588a5f30253262e73f1
mattdennewitz <mattdennewitz@gmail.com>
2012-03-28 16:47:02 -0500

    added setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='python-spotify-metadata-api',
+      version='0.1',
+      description='Lightweight Spotify metadata search API wrapper',
+      author='Matt Dennewitz',
+      author_email='mattdennwitz@gmail.com',
+      packages=['spotify_api'],
+      )
