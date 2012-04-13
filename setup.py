f6fb3c95958992415a6dd7f3c9e19c5cb15bb427
mattdennewitz <mattdennewitz@gmail.com>
2012-04-13 10:31:21 -0500

    version bump: 0.1.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@
 from distutils.core import setup
 
 setup(name='python-spotify-metadata-api',
-      version='0.1',
+      version='0.1.1',
       description='Lightweight Spotify metadata search API wrapper',
       author='Matt Dennewitz',
       author_email='mattdennwitz@gmail.com',
